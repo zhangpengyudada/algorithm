578cf493eb970b23c57c6e95dc522d3dfbef5b6e
zhangpy1 <zhangpy1@guahao.com>
2020-06-04 17:00:38 +0800

    算法

--- src/main/java/Reverse.java
+++ src/main/java/Reverse.java
@@ -3,10 +3,13 @@ import java.util.Arrays;
 public class Reverse {
     public  int reverse(int y){
         int reverseSum=0;
+        //不等于0才走下边
         while (y!=0){
             int pop = y % 10;
+            //reverseSum  大于最大值或者reverseSum大于最大值-7
             if(reverseSum>Integer.MAX_VALUE|| (reverseSum == Integer.MAX_VALUE / 10 && pop > 7) ){
                 return 0;
+                //reverseSum  小于最小值或者reverseSum大于最小值加-8
             }else if(reverseSum < Integer.MIN_VALUE / 10 || (reverseSum == Integer.MIN_VALUE / 10 && pop < -8)){
                 return 0;
             }
--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -5,11 +5,16 @@ import java.util.Map;
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> map=new HashMap<Integer,Integer>();
+        //for循环数组
         for(int i=0;i<nums.length;i++){
+            //先用指定的总和减去数组里的值，如果这个值在map里，则符合要求
             int count = target-nums[i];
+            //检查map里key键是否有这个值
             if(map.containsKey(count)){
+                //如果有的话，说明符合要求
                 return new int[] { map.get(count), i };
             }
+            //没有的话，用值当map的key键，用数组的key当value键
             map.put(nums[i],i);
         }
         throw new IllegalArgumentException("无这两个参数组合");
