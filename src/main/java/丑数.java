fe95c298e36262c3c5ad3b23951a37243962b4da
zhangpy1 <zhangpy1@guahao.com>
2020-07-08 11:18:39 +0800

    丑数解题思路

--- src/main/java/丑数.java
+++ src/main/java/丑数.java
@@ -1,5 +1,8 @@
 public class 丑数 {
     public boolean isUgly(int num) {
+        /*
+        思路：创建一个带2，3，5的数组，然后让n对他们取余，如果取余为0，则表明能整除，除到最后，如果n为1，则表明这个数字n为丑数
+         */
         int[] nums={2,3,5};
         for (int n:nums){
             while (num%n==0){
